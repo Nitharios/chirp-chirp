03203396fb2fb27f270b00263e9b6dc913e344c6
Nathan N <Nitharios@users.noreply.github.com>
2017-09-15 14:50:55 -1000

    All tests passing

--- js/chirp.js
+++ js/chirp.js
@@ -1,5 +1,11 @@
 function chirp(n){
   // FIX ME
+  // var stringChirp = null;
+  if (n === 1) {
+    return ' chirp'
+  } else {
+    return ' chirp' + chirp(--n);
+  }
 }
 
 $(document).ready(function(){
